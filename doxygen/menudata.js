0a3ff3c42a0bd118cb6cf36f73e5832512aa30e4
Bernd Schubert <bernd@bsbernd.com>
2025-03-24 13:47:14 +0100

    Re-generated doxygen documentation

--- doxygen/menudata.js
+++ doxygen/menudata.js
@@ -82,4 +82,5 @@ var menudata={children:[
 {text:"Enumerations",url:"globals_enum.html"},
 {text:"Enumerator",url:"globals_eval.html",children:[
 {text:"f",url:"globals_eval.html#index_f"}]},
-{text:"Macros",url:"globals_defs.html"}]}]}]}
+{text:"Macros",url:"globals_defs.html",children:[
+{text:"f",url:"globals_defs.html#index_f"}]}]}]}]}
